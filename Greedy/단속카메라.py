be98d9d2b779ba71dedbb6bfef0e497739b14401
SYLAR <sokon954@naver.com>
2021-01-25 20:16:21 +0900

    Greedy

--- Greedy/단속카메라.py
+++ Greedy/단속카메라.py
@@ -8,11 +8,11 @@ Created on Mon Jan 25 19:49:18 2021
 def solution(routes):
     routes.sort()
     cameras=[]
-    delt_with=[]
+    dealt_with=[]
     for route in routes:
         if not cameras:
             cameras.append(route.copy())
-            delt_with.append(route.copy())
+            dealt_with.append(route.copy())
         else:
             for cam in cameras:
                 if cam[0]>route[1] or cam[1]<route[0]:
@@ -20,11 +20,11 @@ def solution(routes):
                 elif cam[0]<=route[0] or cam[1]>=route[1]:
                     cam[0] = max(cam[0], route[0])
                     cam[1] = min(cam[1], route[1])
-                    delt_with.append(route)
+                    dealt_with.append(route)
                     break
-            if route not in delt_with:
+            if route not in dealt_with:
                 cameras.append(route)
-                delt_with.append(route)
+                dealt_with.append(route)
     return len(cameras)
 
 routes = [[-20,15], [-14,-5], [-18,-13], [-5,-3]]
